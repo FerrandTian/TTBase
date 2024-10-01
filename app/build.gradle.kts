f3e67949f7015282b528727b0d1076453b37c668
tianfeng <tinytianfeng@gmail.com>
2024-10-01 17:57:07 +0800

    Upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tt.base"
         minSdk = 26
         targetSdk = 35
-        versionCode = 110
-        versionName = "1.1.0"
+        versionCode = 111
+        versionName = "1.1.1"
     }
 
     buildTypes {
